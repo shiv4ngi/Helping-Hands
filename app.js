659f4e0b67a18653280c9fc18ba70af39372d4b6
shiv4ngi <come2shivangu@gmail.com>
2021-05-24 14:07:29 +0530

    Add procfile and update ports

--- app.js
+++ app.js
@@ -303,11 +303,11 @@ app.get('/logout', function(req,res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-var port = process.env.PORT || '3000'
-app.listen(port, err => {
-  if (err)
-    throw err
-  console.log('Server listening on port', port)
-})
+app.listen(port, function() {
+    console.log("Server has started successfully");
+});

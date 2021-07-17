cedf6e6ec4de028f3310425e4b719792fba710f4
shiv4ngi <come2shivangu@gmail.com>
2021-07-17 12:27:55 +0530

    Add note to the caretaker registeration

--- app.js
+++ app.js
@@ -153,6 +153,7 @@ app.post('/caretakerRegister', upload.single('imgURL'), (req, res, next) => {
     password: req.body.password,
     note: req.body.note
   }
+  console.log(obj);
 
   caretakerModel.create(obj, (err, item) => {
     if (err) {
